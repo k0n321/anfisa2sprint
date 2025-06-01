86fbc95056d54ade0e4bd25dc9aa6e98da0a5a60
Aleksei Demenok <lebumaga@gmail.com>
2025-06-01 23:46:01 +0200

    Add initial migration for ice cream models and seed database with categories, toppings, wrappers, and ice creams
    - Created models: Category, Topping, Wrapper, IceCream
    - Added initial migration file for the ice cream app
    - Seeded database with sample data for categories, toppings, wrappers, and various ice cream flavors

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,48 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
-# Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category, Topping, Wrapper, IceCream
+
+# ...и регистрируем её в админке:
+
+
+admin.site.register(Topping) 
+
+admin.site.register(Wrapper) 
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+admin.site.register(IceCream, IceCreamAdmin)
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',        
+    )
+
+admin.site.register(Category, CategoryAdmin)
