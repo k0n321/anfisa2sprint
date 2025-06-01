487de364666ff5cd9b2196afc136d5698855687c
Aleksei Demenok <lebumaga@gmail.com>
2025-06-01 23:52:00 +0200

    Refactor index view to return only necessary ice cream fields and update template for simplified display

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,14 +1,13 @@
+# homepage/views.py
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Запрос:
-    ice_cream_list = IceCream.objects.all()
-    # Полученный из БД QuerySet передаём в словарь контекста:
+    # Возьмём нужное. А ненужное не возьмём:
+    ice_cream_list = IceCream.objects.values('id', 'title')
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
     return render(request, template_name, context)
